b18a31211d130ed377cdaefd3bf8893c45f4640a
SilverComet7 <841531691@qq.com>
2022-08-04 18:45:15 +0800

    feat: fork  yolov5-DNF  and  custom train

---
+++ wx-read/testCsv.py
@@ -0,0 +1,6 @@
+import csv
+with open('eggs.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
